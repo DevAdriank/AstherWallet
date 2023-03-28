a2e398fd44b5fa327cac753ab984b9e0a6d541d7
Kerlyn Adrian Rojas <97488853+DevAdriank@users.noreply.github.com>
2023-03-28 10:26:03 -0400

    AstherWallet App V.1.0.0 (Test)

---
+++ js/js_ad.js
@@ -0,0 +1,16 @@
+var enlaces = document.querySelectorAll('.scroll');
+
+enlaces.forEach(function(enlace) {
+    enlace.addEventListener('click', function(e) {
+        // Evitar que el enlace se comporte como predeterminado
+        e.preventDefault();
+
+        // Obtener el destino del enlace
+        var destino = this.getAttribute('href');
+
+        // Hacer el desplazamiento suave a ese destino
+        document.querySelector(destino).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
